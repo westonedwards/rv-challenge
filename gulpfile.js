82321d1896beb674d3f2e533e8d481667b92e1ac
Weston <westonedwards@gmail.com>
2018-03-11 22:26:01 -0400

    Added JS and CSS minifiers

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,26 @@ const gulp = require('gulp'),
     less = require('gulp-less'),
     jshint = require('gulp-jshint'),
     watch = require('gulp-watch'),
-    browserSync = require('browser-sync').create();
+    browserSync = require('browser-sync').create(),
+    rename = require("gulp-rename"),
+    uglify = require('gulp-uglify-es').default,
+    concat = require('gulp-concat'),
+    cleanCSS = require('gulp-clean-css');
+
+gulp.task('minify-css', () => {
+    gulp.src('./public/styles/src/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./public/styles/'));
+});
+
+// Uglify JS files
+gulp.task("uglify", () => {
+    gulp.src('./public/scripts/src/*.js')
+    .pipe(concat('bundle.min.js'))
+    .pipe(rename('bundle.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./public/scripts/'));
+});
 
 // Process less files into CSS and reload
 gulp.task('less', () => {
